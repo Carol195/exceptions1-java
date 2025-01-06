b354fa929efcc931f46dc5d4d26289dda58ec3b8
Ana Lessa <analessaon@gmail.com>
2025-01-06 13:09:29 -0300

    Classes Entities

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,58 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.SimpleFormatter;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkout;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(Date checkin, Date checkout, Integer roomNumber) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public long duration(){
+        long diff = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkin, Date checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString() {
+        return "Room: " + roomNumber
+                + ", Check-in: " + sdf.format(checkin)
+                + ", Check-out: " + sdf.format(checkout)
+                + ", Duration: " + duration() + " nights";
+    }
+
+}
